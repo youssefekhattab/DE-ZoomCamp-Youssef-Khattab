98cb5e5649c54606925e8838aca09439a6e6bb4d
youssefekhattab <110461019+youssefekhattab@users.noreply.github.com>
2024-04-01 02:19:37 +0200

    Update schema.py

--- project/5- airflow/dags/schema.py
+++ project/5- airflow/dags/schema.py
@@ -1,15 +1,30 @@
 schema = {
     "Sales_data" : [
-      {"invoice_no": str(row[0]),
-            "customer_id": str(row[1]),
-            "gender": str(row[2]),
-            "age": int(row[3]),
-            "category": str(row[4]),
-            "quantity": str(row[5]),
-            "price": float(row[6]),
-            "payment_method": str(row[7]),
-            "invoice_date": datetime.strptime(row[8], '%d/%m/%Y'),
-            "shopping_mall": str(row[9])
+      {"ORDERNUMBER": int(row[0]),
+            "QUANTITYORDERED": int(row[1]),
+            "PRICEEACH": float(row[2]),
+            "ORDERLINENUMBER": int(row[3]),
+            "SALES": str(row[4]),
+            "ORDERDATE": datetime.strptime(row[8], '%d/%m/%Y/%H/%M'),
+            "STATUS": str(row[5]),
+            "QTR_ID": float(row[6]),
+            "MONTH_ID": int(row[7]),
+            "YEAR_ID": int(row[7]),
+            "PRODUCTLINE": str(row[7]),
+            "MSRP": int(row[7]),
+            "PRODUCTCODE": str(row[7]),
+            "CUSTOMERNAME": str(row[7]),
+            "PHONE": str(row[7]),
+            "ADDRESSLINE1": str(row[7]),
+            "ADDRESSLINE2": str(row[7]),
+            "CITY": str(row[7]),
+            "STATE": str(row[9])
+            "POSTALCODE": int(row[7]),
+            "COUNTRY": str(row[7]),
+            "TERRITORY": str(row[7]),
+            "CONTACTLASTNAME": str(row[7]),
+            "CONTACTFIRSTNAME": str(row[7]),
+            "DEALSIZE": str(row[7]),
             }
 
     ]
