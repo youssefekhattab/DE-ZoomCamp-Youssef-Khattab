b2f6ae463371bc9d5ad9d563348731b0d6f9746c
youssefekhattab <110461019+youssefekhattab@users.noreply.github.com>
2024-04-01 02:06:29 +0200

    Update schema.py

--- project/5- airflow/dags/schema.py
+++ project/5- airflow/dags/schema.py
@@ -1,14 +1,16 @@
 schema = {
-    "customer_shopping_data" : [
-        {"name": "invoice_no", "type": "STRING", "mode": "NULLABLE"},
-        {"name": "customer_id", "type": "STRING", "mode": "NULLABLE"},
-        {"name": "gender", "type": "STRING", "mode": "NULLABLE"},
-        {"name": "age", "type": "INTEGER", "mode": "NULLABLE"},
-        {"name": "category", "type": "STRING", "mode": "NULLABLE"},
-        {"name": "quantity", "type": "INTEGER", "mode": "NULLABLE"},
-        {"name": "price", "type": "FLOAT64", "mode": "NULLABLE"},
-        {"name": "payment_method", "type": "STRING", "mode": "NULLABLE"},
-        {"name": "invoice_date", "type": "TIMESTAMP", "mode": "NULLABLE"},
-        {"name": "shopping_mall", "type": "STRING", "mode": "NULLABLE"}
+    "Sales_data" : [
+      {"invoice_no": str(row[0]),
+            "customer_id": str(row[1]),
+            "gender": str(row[2]),
+            "age": int(row[3]),
+            "category": str(row[4]),
+            "quantity": str(row[5]),
+            "price": float(row[6]),
+            "payment_method": str(row[7]),
+            "invoice_date": datetime.strptime(row[8], '%d/%m/%Y'),
+            "shopping_mall": str(row[9])
+            }
+
     ]
 }
