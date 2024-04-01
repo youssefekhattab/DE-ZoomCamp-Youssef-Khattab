9bc6f5c240af33b3ffd589869f7655852a8c0931
youssefekhattab <110461019+youssefekhattab@users.noreply.github.com>
2024-04-01 02:18:43 +0200

    Update Sales_data.sql

--- project/5- airflow/dags/sql/Sales_data.sql
+++ project/5- airflow/dags/sql/Sales_data.sql
@@ -1,18 +1,8 @@
 INSERT {{ BIGQUERY_DATASET }}.{{ CUSTOMER_SHOPPING_DATA_TABLE }}
 SELECT
-    COALESCE(customer_id, 'NA') AS customer_id,
-    COALESCE(gender, 'NA') AS gender,
-    COALESCE(gender, 'NA') AS gender,
-    COALESCE(age, -1) AS age,
-    COALESCE(category, 'NA') AS category,
-    COALESCE(quantity, -1) AS quantity,
-    COALESCE(price, -1.0) AS price,
-    COALESCE(payment_method, 'NA') AS payment_method,
-    invoice_date AS invoice_date,
-    COALESCE(shopping_mall, 'NA') AS shopping_mall,
+   *
 
-FROM {{ BIGQUERY_DATASET }}.{{ CUSTOMER_SHOPPING_DATA_TABLE}}_{{ logical_date.strftime("%m%d%H") }} -- Creates a table name with month day and hour values appended to it
-                                                                                      -- like customer_shopping_data.json_032313 for 23-03-2022 13:00:00
+FROM {{ BIGQUERY_DATASET }}.{{ Sales_data_DATA_TABLE}}_{{ logical_date.strftime("%m%d%H") }} 
 {"Sales_data": "ORDERNUMBER": int(row[0]),
             "QUANTITYORDERED": int(row[1]),
             "PRICEEACH": float(row[2]),
